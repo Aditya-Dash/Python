2284cdf0f03dbaebb4d5b27af6e16f0d5ca45c7e
Aditya-Dash <79528810+Aditya-Dash@users.noreply.github.com>
2021-02-23 21:12:40 +0530

    Python basics
    Coding practice based on Udemy and python official documentation site

---
+++ Python Basics/Udemy_course/cap.py
@@ -0,0 +1,13 @@
+def cap_text(text):
+    '''
+    Input a string
+    Output the capitalize text
+    '''
+    return text.capitalize()
+    
+def cap_title(text):
+    '''
+    Input a string
+    Output the capitalize text
+    '''
+    return text.title()
---
+++ Python Basics/Udemy_course/pylint_test.py
@@ -0,0 +1,12 @@
+'''
+Simple script for pylint
+pyliny <filename>.py -r y
+'''
+def print_report():
+    '''
+    Test Method
+    '''
+    first = 60
+    second = 'data'
+    print(first)
+    print(second)
